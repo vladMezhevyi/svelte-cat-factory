d3decd5975235b255e72ea73d1cf2c84bd87b9fe
vladMezhevyi <vlad.mezhevyi@gmail.com>
2026-07-14 23:06:56 +0100

    refactor: use tailwind and shadcn for footer styles

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -6,53 +6,27 @@
     PUBLIC_API_NAME
   } from '$env/static/public';
 
+  import { DotIcon } from 'phosphor-svelte';
+
   const rel = 'external noopener noreferrer';
+  const target = '_blank';
+  const className = 'text-primary hover:underline';
 </script>
 
-<footer class="footer">
+<footer
+  class="bg-secondary text-secondary-foreground text-xs sm:text-base flex items-center px-4 py-2 gap-1 sm:gap-2 flex-wrap"
+>
   <p>
     Made by
-    <a href={PUBLIC_GITHUB_PROFILE_URL} target="_blank" {rel}>{PUBLIC_OWNER_NAME}</a>
+    <a href={PUBLIC_GITHUB_PROFILE_URL} class={className} {rel} {target}>
+      {PUBLIC_OWNER_NAME}
+    </a>
   </p>
 
-  <span class="separator">&middot;</span>
+  <DotIcon size="2rem" class="text-secondary-foreground text-base" />
 
   <p>
     Cat magic by
-    <a href={PUBLIC_API_URL} target="_blank" {rel}>{PUBLIC_API_NAME}</a>
+    <a href={PUBLIC_API_URL} class={className} {rel} {target}>{PUBLIC_API_NAME}</a>
   </p>
 </footer>
-
-<style>
-  .footer {
-    display: flex;
-    align-items: center;
-    gap: 1rem;
-    padding: 1rem;
-    background-color: darkslategray;
-  }
-
-  .footer > p {
-    color: #fff;
-  }
-
-  .footer a {
-    color: darkturquoise;
-  }
-
-  .footer a:hover {
-    text-decoration: none;
-  }
-
-  .separator {
-    font-size: 18px;
-    font-weight: 700;
-    color: #fff;
-  }
-
-  @media (max-width: 768px) {
-    .footer {
-      padding: 0.5rem;
-    }
-  }
-</style>
